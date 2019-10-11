f5a2ede11bbb4679861938a48098dfc3f46de617
Jairo <jairoh28@gmail.com>
2019-10-11 18:47:24 -0500

    Changes the getRandomQuote function to not have repeats while looping through quotes array.
    Also edited quotes array and comments to cite sources.

--- js/script.js
+++ js/script.js
@@ -15,20 +15,20 @@ quotes = [
   {
     quote: 'Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.',
     source: 'Michael Scott',
-    citation: 'The Office, Season 5, "The Duel"',
+    citation: 'The Office',
     year: 2009
   },
   {
     quote: 'I’m not superstitious, but I am a little stitious.',
     source: 'Michael Scott',
-    citation: 'The Office, Season 4, "Fun Run"',
-    year: 2007
+    citation: 'The Office',
+    year: 2007,
+    tag: 'Humor'
   },
   {
-    quote: 'Without music, life would be a mistake.',
-    source: 'Friedrich Nietzsche',
-    citation: 'Twilight of the Idols',
-    year: 1889
+    quote: 'What do you think people did before ladders?',
+    source: 'Hannibal Buress',
+    citation: 'The Eric Andre Show'
   },
   {
     quote: 'Jump, and you will find out how to unfold your wings as you fall.',
@@ -41,38 +41,74 @@ quotes = [
   },
   {
     quote: 'The privilege of a lifetime is to become who you truly are.',
-    source: 'Carl Jung'
+    source: 'Carl Jung',
+    tag: 'Psychology'
   },
   {
-    quote: 'It take a long time to become young',
-    source: 'Pablo Picasso'
+    quote: 'It take a long time to become young.',
+    source: 'Pablo Picasso',
+    tag: 'Philosophy'
   },
   {
-    quote: 'The most beautiful thing we can experience is the mysterious. It is the source of all true art and science.',
-    source: 'Albert Einstein'
+    quote: 'All religions, arts and sciences are branches of the same tree.',
+    source: 'Albert Einstein',
+    tag: 'Philosophy'
   },
   {
-    quote: 'The further one goes, the less one knows.',
-    source: 'Lao Tzu',
-    citation: 'Tao Te Ching',
-    year: '4th century BC'
+    quote: 'Got more soul than a sock with a hole.',
+    source: 'MF DOOM',
+    citation: 'Rhinestone Cowboy, Madvilliany',
+    year: 2004,
+    tag: 'Music'
+  },
+  {
+    quote: 'Catch a throatful from the fire vocal, Ash and molten glass like Eyjafjallajökull.',
+    source: 'JJ DOOM',
+    citation: 'GUV’NOR',
+    tag: 'Music'
   },
   {
     quote: 'Fill your bowl to the brim and it will spill. Keep sharpening your knife and it will blunt.',
     source: 'Lao Tzu',
     citation: 'Tao Te Ching',
     year: '4th century BC'
+  },
+  {
+    quote: 'Nature uses only the longest threads to weave her patterns, so that each small piece of her fabric reveals the organization of the entire tapestry.',
+    source: 'Richard Feynman',
+    tag: 'Physics'
+  },
+  {
+    quote: 'DJ KHALED',
+    source: 'DJ Khaled',
+    tag: 'Motivational'
+  },
+  {
+    quote: 'Legalize Ranch.',
+    source: 'Eric Andre',
+    citation: 'The Eric Andre Show'
+  },
+  {
+    quote: 'See you, space cowboy...',
+    source: 'Cowboy Bebop'
+  },
+  {
+    quote: 'I’d like to point out to people the divine in a musical language that transcends words. I want to speak to their souls.',
+    source: 'John Coltrane',
+    tag: 'Music'
   }
 ]
 
-// getRandomQuote function creates a random integer less than the length of the 'quotes' array to return a random quote object from the array.
+// getRandomQuote function creates a random integer in range of the length of the 'quotes' array to return a random quote object from the array.
 
 function getRandomQuote () {
-  randomNumber = Math.floor(Math.random() * quotes.length);
-  return quotes[randomNumber];
+  for (i = quotes.length; i--;) {
+    var random = quotes.splice(Math.floor(Math.random() * i), 1)[0];
+    return random;
+  };
 }
 
-// backgroundCol function that changes background color
+// backgroundCol function that changes background color. Source: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
 
 function backgroundCol () {
   blue = Math.floor(Math.random() * 256);
@@ -82,19 +118,26 @@ function backgroundCol () {
   document.body.style.backgroundColor = color;
 }
 
-// printQuote function creates the HTML string to be printed out and also calls for the properties if they are contained in the quote object.
+/*
+printQuote function creates the HTML string to be printed out and also calls for the properties if they are contained in the quote object.
+Function includes setInterval() & clearInterval() functions to auto-refresh the quote after 20 seconds of being idle. Source: https://www.w3schools.com/jsref/met_win_setinterval.asp
+Function also generates a change in background color when called.
+*/
 
 function printQuote () {
   backgroundCol();
   var randomQuote = getRandomQuote();
   var message = '';
-  message += '<p class="quote">' + randomQuote.quote + '</p>';
+  message += '<p class="quote" id="quote">' + randomQuote.quote + '</p>';
   message += '<p class="source">' + randomQuote.source;
   if (randomQuote.citation) {
-    message += '<span class="citation">' + randomQuote.citation + '</span>';
+    message += '<span class="citation">' + randomQuote.citation + ' </span>';
   }
   if (randomQuote.year) {
-    message += '<span class="year">' + randomQuote.year + '</span>';
+    message += '<span class="year">' + randomQuote.year + ' </span>';
+  }
+  if (randomQuote.tag) {
+    message += '<span class="tag">, ' + randomQuote.tag + '</span>';
   }
   message += '</p>';
   div = document.getElementById('quote-box');
